39709f983f4c82eaad209b3ee3b5122aeb2e96ed
Cameron Dixon <cam.dixon@gmail.com>
2014-04-01 23:13:26 +0000

    update migration for lessons table

--- db/migrate/20140401230504_lessons.rb
+++ db/migrate/20140401230504_lessons.rb
@@ -1,14 +1,13 @@
 class Lessons < ActiveRecord::Migration
   def change
-    def change
-      create_table :lessons do |t|
-        t.references :student
-        t.timestamps
-        t.date :attended_on
-        t.integer :time_of_day
-        t.boolean :cancelled
-        t.boolean :late
-        t.boolean :reschedule
+    create_table :lessons do |t|
+      t.references :student
+      t.timestamps
+      t.date :attended_on
+      t.integer :time_of_day
+      t.boolean :cancelled
+      t.boolean :late
+      t.boolean :reschedule
     end
   end
 end
