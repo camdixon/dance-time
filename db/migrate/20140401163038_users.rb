a80c342c9cd9ff1a6c76bf422eddfc5eb4b58dd1
Cameron Dixon <cam.dixon@gmail.com>
2014-04-01 16:31:20 +0000

    add in a migration for users table

---
+++ db/migrate/20140401163038_users.rb
@@ -0,0 +1,10 @@
+class Users < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string "name"
+      t.string "role"
+      t.string "email"
+      t.string "image_url"
+      t.string "password_digest"
+  end
+end
