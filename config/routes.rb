d30544265e6e127279ae3deade1c86572e6c7e98
Cameron Dixon <cam.dixon@gmail.com>
2014-04-01 23:44:35 +0000

    update routes.rb file to add lessons for students and users / teachers

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ NkuProject::Application.routes.draw do
   resources :users   
   resources :sessions
   resources :students
+  resources :lessons
   
   # You can have the root of your site routed with "root"
   root 'welcome#index'
