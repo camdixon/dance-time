d517a3f51fa844d8dcea1a8f47cc33969e8936f4
Cameron Dixon <cam.dixon@gmail.com>
2014-04-01 16:32:00 +0000

    add migration for students table

---
+++ db/migrate/20140401163140_students.rb
@@ -0,0 +1,19 @@
+class Students < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string "first_name"
+      t.string "last_name"
+      t.string "cell_phone"
+      t.string "home_phone"
+      t.string "email"
+      t.string "partner_first_name"
+      t.string "partner_last_name"
+      t.string "partner_cell_phone"
+      t.string "partner_home_phone"
+      t.string "partner_email"
+      t.text :description
+      t.string "image_url"
+      t.boolean :active
+      t.string "type"
+  end
+end
