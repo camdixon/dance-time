89e05b464c096d8608d1bdbaa042e822d3a003ec
Cameron Dixon <cam.dixon@gmail.com>
2014-05-21 20:03:23 +0000

    add student's show page to day view to click on their name and see additional info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520170814) do
+ActiveRecord::Schema.define(version: 20140521195106) do
 
   create_table "lessons", force: true do |t|
     t.integer  "student_id"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20140520170814) do
     t.string   "day"
     t.datetime "start_time"
     t.datetime "end_time"
+    t.boolean  "no_show"
+    t.boolean  "charged"
   end
 
   create_table "students", force: true do |t|
